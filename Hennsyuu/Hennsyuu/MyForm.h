da6cb26e33b7658f68aa4ca043e4d830e2144e79
kawashima <kawashima-yu@tounichi-g.co.jp>
2024-05-22 15:33:29 +0900

    ほじごんのままはほじまま

--- Hennsyuu/Hennsyuu/MyForm.h
+++ Hennsyuu/Hennsyuu/MyForm.h
@@ -149,9 +149,10 @@ namespace Hennsyuu {
 			// back
 			// 
 			this->back->BackColor = System::Drawing::Color::Yellow;
-			this->back->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->back->FlatStyle = System::Windows::Forms::FlatStyle::Popup;
+			this->back->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 18, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(128)));
-			this->back->Location = System::Drawing::Point(109, 650);
+			this->back->Location = System::Drawing::Point(60, 650);
 			this->back->Name = L"back";
 			this->back->Size = System::Drawing::Size(135, 65);
 			this->back->TabIndex = 49;
@@ -160,8 +161,9 @@ namespace Hennsyuu {
 			// 
 			// logout
 			// 
-			this->logout->BackColor = System::Drawing::Color::Tomato;
-			this->logout->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->logout->BackColor = System::Drawing::Color::Orange;
+			this->logout->FlatStyle = System::Windows::Forms::FlatStyle::Popup;
+			this->logout->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(128)));
 			this->logout->Location = System::Drawing::Point(794, 38);
 			this->logout->Name = L"logout";
@@ -173,6 +175,7 @@ namespace Hennsyuu {
 			// label7
 			// 
 			this->label7->AutoSize = true;
+			this->label7->ForeColor = System::Drawing::Color::Transparent;
 			this->label7->Location = System::Drawing::Point(329, 396);
 			this->label7->Name = L"label7";
 			this->label7->Size = System::Drawing::Size(15, 15);
@@ -182,6 +185,7 @@ namespace Hennsyuu {
 			// label6
 			// 
 			this->label6->AutoSize = true;
+			this->label6->ForeColor = System::Drawing::Color::Transparent;
 			this->label6->Location = System::Drawing::Point(329, 349);
 			this->label6->Name = L"label6";
 			this->label6->Size = System::Drawing::Size(15, 15);
@@ -191,6 +195,7 @@ namespace Hennsyuu {
 			// label5
 			// 
 			this->label5->AutoSize = true;
+			this->label5->ForeColor = System::Drawing::Color::Transparent;
 			this->label5->Location = System::Drawing::Point(329, 305);
 			this->label5->Name = L"label5";
 			this->label5->Size = System::Drawing::Size(15, 15);
@@ -200,6 +205,7 @@ namespace Hennsyuu {
 			// label4
 			// 
 			this->label4->AutoSize = true;
+			this->label4->ForeColor = System::Drawing::Color::Transparent;
 			this->label4->Location = System::Drawing::Point(329, 269);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(15, 15);
@@ -209,6 +215,7 @@ namespace Hennsyuu {
 			// label3
 			// 
 			this->label3->AutoSize = true;
+			this->label3->ForeColor = System::Drawing::Color::Transparent;
 			this->label3->Location = System::Drawing::Point(329, 234);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(15, 15);
@@ -218,6 +225,7 @@ namespace Hennsyuu {
 			// label2
 			// 
 			this->label2->AutoSize = true;
+			this->label2->ForeColor = System::Drawing::Color::Transparent;
 			this->label2->Location = System::Drawing::Point(329, 190);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(15, 15);
@@ -227,6 +235,7 @@ namespace Hennsyuu {
 			// label1
 			// 
 			this->label1->AutoSize = true;
+			this->label1->ForeColor = System::Drawing::Color::Transparent;
 			this->label1->Location = System::Drawing::Point(329, 148);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(15, 15);
@@ -236,7 +245,8 @@ namespace Hennsyuu {
 			// update
 			// 
 			this->update->BackColor = System::Drawing::Color::Yellow;
-			this->update->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->update->FlatStyle = System::Windows::Forms::FlatStyle::Popup;
+			this->update->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 18, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(128)));
 			this->update->Location = System::Drawing::Point(794, 650);
 			this->update->Name = L"update";
@@ -250,7 +260,7 @@ namespace Hennsyuu {
 			this->textBox_memo->Location = System::Drawing::Point(350, 392);
 			this->textBox_memo->Multiline = true;
 			this->textBox_memo->Name = L"textBox_memo";
-			this->textBox_memo->Size = System::Drawing::Size(350, 238);
+			this->textBox_memo->Size = System::Drawing::Size(350, 133);
 			this->textBox_memo->TabIndex = 39;
 			// 
 			// textBox_assignee
@@ -283,7 +293,7 @@ namespace Hennsyuu {
 			// 
 			// textBox_title
 			// 
-			this->textBox_title->Location = System::Drawing::Point(350, 183);
+			this->textBox_title->Location = System::Drawing::Point(350, 190);
 			this->textBox_title->Name = L"textBox_title";
 			this->textBox_title->Size = System::Drawing::Size(350, 22);
 			this->textBox_title->TabIndex = 34;
@@ -298,86 +308,94 @@ namespace Hennsyuu {
 			// memo
 			// 
 			this->memo->AutoSize = true;
-			this->memo->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->memo->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(128)));
+			this->memo->ForeColor = System::Drawing::Color::Transparent;
 			this->memo->Location = System::Drawing::Point(265, 388);
 			this->memo->Name = L"memo";
-			this->memo->Size = System::Drawing::Size(43, 23);
+			this->memo->Size = System::Drawing::Size(45, 23);
 			this->memo->TabIndex = 32;
 			this->memo->Text = L"メモ";
 			// 
 			// assignee
 			// 
 			this->assignee->AutoSize = true;
-			this->assignee->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->assignee->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(128)));
+			this->assignee->ForeColor = System::Drawing::Color::Transparent;
 			this->assignee->Location = System::Drawing::Point(229, 342);
 			this->assignee->Name = L"assignee";
-			this->assignee->Size = System::Drawing::Size(79, 23);
+			this->assignee->Size = System::Drawing::Size(82, 23);
 			this->assignee->TabIndex = 31;
 			this->assignee->Text = L"担当者";
 			// 
 			// location
 			// 
 			this->location->AutoSize = true;
-			this->location->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->location->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(128)));
+			this->location->ForeColor = System::Drawing::Color::Transparent;
 			this->location->Location = System::Drawing::Point(253, 301);
 			this->location->Name = L"location";
-			this->location->Size = System::Drawing::Size(56, 23);
+			this->location->Size = System::Drawing::Size(58, 23);
 			this->location->TabIndex = 30;
 			this->location->Text = L"場所";
 			// 
 			// finish
 			// 
 			this->finish->AutoSize = true;
-			this->finish->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->finish->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(128)));
+			this->finish->ForeColor = System::Drawing::Color::Transparent;
 			this->finish->Location = System::Drawing::Point(206, 262);
 			this->finish->Name = L"finish";
-			this->finish->Size = System::Drawing::Size(102, 23);
+			this->finish->Size = System::Drawing::Size(106, 23);
 			this->finish->TabIndex = 29;
 			this->finish->Text = L"終了日時";
 			// 
 			// start
 			// 
 			this->start->AutoSize = true;
-			this->start->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->start->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(128)));
+			this->start->ForeColor = System::Drawing::Color::Transparent;
 			this->start->Location = System::Drawing::Point(207, 226);
 			this->start->Name = L"start";
-			this->start->Size = System::Drawing::Size(102, 23);
+			this->start->Size = System::Drawing::Size(106, 23);
 			this->start->TabIndex = 28;
 			this->start->Text = L"開始日時";
 			// 
 			// s_title
 			// 
 			this->s_title->AutoSize = true;
-			this->s_title->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->s_title->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(128)));
+			this->s_title->ForeColor = System::Drawing::Color::Transparent;
 			this->s_title->Location = System::Drawing::Point(231, 183);
 			this->s_title->Name = L"s_title";
-			this->s_title->Size = System::Drawing::Size(78, 23);
+			this->s_title->Size = System::Drawing::Size(82, 23);
 			this->s_title->TabIndex = 27;
 			this->s_title->Text = L"タイトル";
 			// 
 			// ID
 			// 
 			this->ID->AutoSize = true;
-			this->ID->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->ID->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 16.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(128)));
+			this->ID->ForeColor = System::Drawing::Color::Transparent;
 			this->ID->Location = System::Drawing::Point(272, 142);
 			this->ID->Name = L"ID";
-			this->ID->Size = System::Drawing::Size(37, 28);
+			this->ID->Size = System::Drawing::Size(39, 28);
 			this->ID->TabIndex = 26;
 			this->ID->Text = L"ID";
 			// 
 			// title
 			// 
 			this->title->AutoSize = true;
-			this->title->BackColor = System::Drawing::SystemColors::Control;
+			this->title->BackColor = System::Drawing::SystemColors::MenuHighlight;
 			this->title->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 24, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(128)));
+			this->title->ForeColor = System::Drawing::Color::Transparent;
 			this->title->Location = System::Drawing::Point(53, 45);
 			this->title->Name = L"title";
 			this->title->Size = System::Drawing::Size(292, 40);
@@ -387,29 +405,32 @@ namespace Hennsyuu {
 			// username
 			// 
 			this->username->AutoSize = true;
-			this->username->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->username->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(128)));
-			this->username->Location = System::Drawing::Point(250, 661);
+			this->username->ForeColor = System::Drawing::Color::Transparent;
+			this->username->Location = System::Drawing::Point(199, 587);
 			this->username->Name = L"username";
-			this->username->Size = System::Drawing::Size(109, 23);
+			this->username->Size = System::Drawing::Size(114, 23);
 			this->username->TabIndex = 50;
 			this->username->Text = L"ユーザー名";
 			// 
 			// password
 			// 
 			this->password->AutoSize = true;
-			this->password->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->password->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(128)));
-			this->password->Location = System::Drawing::Point(250, 692);
+			this->password->ForeColor = System::Drawing::Color::Transparent;
+			this->password->Location = System::Drawing::Point(206, 620);
 			this->password->Name = L"password";
-			this->password->Size = System::Drawing::Size(100, 23);
+			this->password->Size = System::Drawing::Size(105, 23);
 			this->password->TabIndex = 51;
 			this->password->Text = L"パスワード";
 			// 
 			// label10
 			// 
 			this->label10->AutoSize = true;
-			this->label10->Location = System::Drawing::Point(366, 668);
+			this->label10->ForeColor = System::Drawing::Color::Transparent;
+			this->label10->Location = System::Drawing::Point(330, 595);
 			this->label10->Name = L"label10";
 			this->label10->Size = System::Drawing::Size(15, 15);
 			this->label10->TabIndex = 52;
@@ -418,7 +439,8 @@ namespace Hennsyuu {
 			// label11
 			// 
 			this->label11->AutoSize = true;
-			this->label11->Location = System::Drawing::Point(366, 700);
+			this->label11->ForeColor = System::Drawing::Color::Transparent;
+			this->label11->Location = System::Drawing::Point(329, 631);
 			this->label11->Name = L"label11";
 			this->label11->Size = System::Drawing::Size(15, 15);
 			this->label11->TabIndex = 53;
@@ -426,23 +448,23 @@ namespace Hennsyuu {
 			// 
 			// textBox_username
 			// 
-			this->textBox_username->Location = System::Drawing::Point(387, 661);
+			this->textBox_username->Location = System::Drawing::Point(350, 588);
 			this->textBox_username->Name = L"textBox_username";
-			this->textBox_username->Size = System::Drawing::Size(313, 22);
+			this->textBox_username->Size = System::Drawing::Size(350, 22);
 			this->textBox_username->TabIndex = 54;
 			// 
 			// textBox_password
 			// 
-			this->textBox_password->Location = System::Drawing::Point(387, 693);
+			this->textBox_password->Location = System::Drawing::Point(350, 624);
 			this->textBox_password->Name = L"textBox_password";
-			this->textBox_password->Size = System::Drawing::Size(313, 22);
+			this->textBox_password->Size = System::Drawing::Size(350, 22);
 			this->textBox_password->TabIndex = 55;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 15);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->BackColor = System::Drawing::SystemColors::ActiveCaption;
+			this->BackColor = System::Drawing::SystemColors::MenuHighlight;
 			this->ClientSize = System::Drawing::Size(982, 753);
 			this->Controls->Add(this->textBox_password);
 			this->Controls->Add(this->textBox_username);
---
+++ hodumi/hodumi/MyForm1.cpp
@@ -0,0 +1,8 @@
+#include "MyForm1.h"
+
+using namespace hodumi;
+using namespace System::IO;
+using namespace System::Text;
+using namespace System::Collections::Generic;
+
+
